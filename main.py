16118176f44e70924499f840643c3d9d3ca98eba
Akv1lon <103238791+Akv1lon@users.noreply.github.com>
2023-07-18 19:35:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-def strcounter(s):
-    for sy in set(s):
-        counter = 0
-        for s_sy in s:
-            if sy == s_sy:
-                counter += 1  
-        print(sy, counter)
+# def strcounter(s):
+#     for sy in set(s):
+#         counter = 0
+#         for s_sy in s:
+#             if sy == s_sy:
+#                 counter += 1  
+#         print(sy, counter)
     
-strcounter('aabbcc')
+# strcounter('aabbcc')
 
 #O(N**2) - 2 цикла(цикл в цикле)
 
-# counter = {}
-# def strcounter(s):
-#     for sym in s:
-#         counter[sym] = counter.get(sym, 0) + 1
+counter = {}
+ def strcounter(s):
+     for sym in s:
+         counter[sym] = counter.get(sym, 0) + 1
         
-#     for sym, count in counter.items():
-#         print(sym, count)
+     for sym, count in counter.items():
+         print(sym, count)
         
-# strcounter('aabbcccdd')
+ strcounter('aabbcccdd')
 
 #O(N) - 1 цикл(2 необяз и может быть вне функции)
